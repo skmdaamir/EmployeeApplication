0e2abbb0a97fd44df587712c9af5d656386d0247
skmdaamir <skmdaamir29@gmail.com>
2021-02-02 18:41:39 +0530

    initial changes

---
+++ employeeapplication/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:employeeapplication/welcome_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return MaterialApp(
+      title: 'Employee Tracking System',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.latoTextTheme(textTheme).copyWith(
+          bodyText1: GoogleFonts.montserrat(textStyle: textTheme.bodyText1),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: WelcomePage(),
+    );
+  }
+}
